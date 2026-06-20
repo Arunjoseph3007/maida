2a6fc53eef36605c9bf5d9857e52b86fe595ad87
arun_forcepoint_laptop <arunjoseph3007@gmail.com>
2026-06-20 12:53:18 +0530

    Undo redo system added

--- text.py
+++ text.py
@@ -1,18 +1,22 @@
 from maida import *
+from dataclasses import dataclass
+from typing import List
+from contextlib import contextmanager
 import base64
 import pathlib
 import os
 import sys
 
 # TODO ctrl backspace, del
-# TODO undo redo
+# TODO syntax highlighting
+
+Buffer = List[str]
 
 
 def dir_entry_sort(k: os.DirEntry[str]):
     if k.is_dir():
         return "0" + k.name
-    else:
-        return "1" + k.name
+    return "1" + k.name
 
 
 class Anchor:
@@ -175,14 +179,74 @@ class Cursor:
             self.start.right(lines)
             self.end.right(lines)
 
+    def copy(self):
+        c = Cursor()
+        c.start.cx = self.start.cx
+        c.start.cy = self.start.cy
+        c.end.cx = self.end.cx
+        c.end.cy = self.end.cy
+        return c
+
+
+@dataclass
+class Edit:
+    start: int
+    plus: Buffer
+    minus: Buffer
+    start_cursor: Cursor = None
+    end_cursor: Cursor = None
+
+    @staticmethod
+    def from_diff(a: Buffer, b: Buffer):
+        if a == b:
+            return Edit(start=0, plus=[], minus=[])
+
+        start = 0
+        while start < len(a) and start < len(b) and a[start] == b[start]:
+            start += 1
+
+        endA = len(a)
+        endB = len(b)
+        while endA > start and endB > start and a[endA - 1] == b[endB - 1]:
+            endA -= 1
+            endB -= 1
+
+        return Edit(start=start, plus=b[start:endB], minus=a[start:endA])
+
+    def undo(self, buffer: Buffer):
+        for _ in self.plus:
+            buffer.pop(self.start)
+
+        for i, l in enumerate(self.minus):
+            buffer.insert(self.start + i, l)
+
+    def redo(self, buffer: Buffer):
+        for _ in self.minus:
+            buffer.pop(self.start)
+
+        for i, l in enumerate(self.plus):
+            buffer.insert(self.start + i, l)
+
+    def __str__(self):
+        return f"Edit(st={self.start}, p={self.plus and self.plus[0]}, m={self.minus and self.minus[0]})"
+
+
+Histroy = List[Edit]
+MAX_UNDO_HISTORY_SIZE = 100
+
 
 class TUICSV(TUI):
     def __init__(self, name):
         super().__init__(fps=30, mouse_mode=MouseMode.ALL_SGR)
-        self.lines = [""]
+        self.lines: Buffer = [""]
+        self.old_lines: Buffer = [""]
         self.tcursor = Cursor()
+        self.old_tcursor = Cursor()
         self.scroll = 0
         self.box_height = 0
+        self.undo_stack: Histroy = []
+        self.redo_stack: Histroy = []
+        self.transaction_ref_count = 0
 
         path = pathlib.Path(name)
         self.load_path(path)
@@ -190,7 +254,7 @@ class TUICSV(TUI):
     def load_path(self, path: pathlib.Path):
         if not path.exists():
             self.lerror(f"Path {path} does not exist")
-        if path.is_dir():
+        elif path.is_dir():
             self.linfo(f"Directory {path} loaded")
             self.load_dir(path)
         elif path.is_file():
@@ -201,16 +265,27 @@ class TUICSV(TUI):
         self.dir = path
         self.file = None
 
-        self.ftree = list(os.scandir(self.dir))
+        exclusion = [".git"]  # not sure if we want this
+        self.ftree = os.scandir(self.dir)
+        self.ftree = [x for x in self.ftree if x.name not in exclusion]
         self.ftree.sort(key=dir_entry_sort)
 
     def load_file(self, path):
         self.dir = None
         self.file = path
-
         self.tcursor = Cursor()
-        with open(self.file) as f:
-            self.lines = f.read().splitlines()
+        self.old_tcursor = Cursor()
+        self.undo_stack.clear()
+        self.redo_stack.clear()
+        self.transaction_ref_count = 0
+        self.lines = [""]
+        self.old_lines = [""]
+
+        try:
+            with open(self.file) as f:
+                self.lines = f.read().splitlines()
+        except Exception:
+            self.lwarn(f"Error when reading file {path}, File might be binary")
 
     def save(self):
         if not self.file:
@@ -238,6 +313,60 @@ class TUICSV(TUI):
         self.tcursor.start.cursor_regulate(self.lines)
         self.tcursor.end.cursor_regulate(self.lines)
 
+    def start_transaction(self):
+        if self.transaction_ref_count == 0:
+            self.old_lines = self.lines.copy()
+            self.old_tcursor = self.tcursor.copy()
+        self.transaction_ref_count += 1
+
+    def end_transaction(self):
+        self.transaction_ref_count -= 1
+
+        if self.transaction_ref_count > 0:
+            return
+
+        if self.lines == self.old_lines:
+            return
+
+        self.redo_stack.clear()
+
+        ed = Edit.from_diff(self.old_lines, self.lines)
+        ed.start_cursor = self.old_tcursor.copy()
+        ed.end_cursor = self.tcursor.copy()
+        self.undo_stack.append(ed)
+        self.old_lines.clear()
+
+        self.undo_stack = self.undo_stack[-MAX_UNDO_HISTORY_SIZE:]
+
+    def transaction(self):
+        @contextmanager
+        def with_transaction():
+            self.start_transaction()
+            yield
+            self.end_transaction()
+
+        return with_transaction()
+
+    def undo(self):
+        if len(self.undo_stack) == 0:
+            return
+
+        ed = self.undo_stack.pop()
+        self.redo_stack.append(ed)
+
+        ed.undo(self.lines)
+        self.tcursor = ed.start_cursor
+
+    def redo(self):
+        if len(self.redo_stack) == 0:
+            return
+
+        ed = self.redo_stack.pop()
+        self.undo_stack.append(ed)
+
+        ed.redo(self.lines)
+        self.tcursor = ed.end_cursor
+
     def render(self):
         ftree_box, box = self.box.left(30)
         self.draw_box(ftree_box)
@@ -255,12 +384,13 @@ class TUICSV(TUI):
         if self.dir:
             for entry in self.ftree:
                 fteb, rest = rest.top(1)
-                label = "> " if entry.is_dir() else "  "
+                label = "❯ " if entry.is_dir() else "  "
                 label += entry.name
                 if button(self, fteb, label, align=TextAlign.LEFT):
                     path = pathlib.Path(self.dir) / pathlib.Path(entry.name)
                     self.load_path(path)
 
+        # editor
         line_no_space = len(str(self.nlines))
 
         cx, cy = self.tcursor.end.pair()
@@ -291,10 +421,13 @@ class TUICSV(TUI):
 
         if self.display_diagnostics:
             diag = box.bottom_right(30, 10)
+            ln = next_line(1)
             self.draw_box(diag)
-            self.add_line(f"is sel - {self.tcursor.is_selection()}", diag, 1)
-            self.add_line(f"start - {self.tcursor.start}", diag, 2)
-            self.add_line(f"end - {self.tcursor.end}", diag, 3)
+            self.add_line(f"is sel - {self.tcursor.is_selection()}", diag, next(ln))
+            self.add_line(f"start - {self.tcursor.start}", diag, next(ln))
+            self.add_line(f"end - {self.tcursor.end}", diag, next(ln))
+            self.add_line(f"Undo len - {len(self.undo_stack)}", diag, next(ln))
+            self.add_line(f"Redo len - {len(self.redo_stack)}", diag, next(ln))
 
     def on_input(self, ch):
         if ch == CTRL + "q":
@@ -355,86 +488,92 @@ class TUICSV(TUI):
             elif ch == SHIFT + Keys.RIGHT:
                 self.tcursor.end.right(self.lines)
 
-        with self.error_logging("editing"):
-            # editing
-            if ch == ALT + Keys.UP and not self.tcursor.is_selection() and self.tcursor.end.cy > 0:
-                x = self.tcursor.end.cy
-                self.lines[x], self.lines[x - 1] = self.lines[x - 1], self.lines[x]
-                self.tcursor.up(self.lines)
-            elif ch == ALT + Keys.DOWN and not self.tcursor.is_selection() and self.tcursor.end.cy < self.nlines - 1:
-                x = self.tcursor.end.cy + 1
-                self.lines[x], self.lines[x - 1] = self.lines[x - 1], self.lines[x]
-                self.tcursor.down(self.lines)
+        with self.transaction():
+            with self.error_logging("editing"):
+                # editing
+                if ch == ALT + Keys.UP and not self.tcursor.is_selection() and self.tcursor.end.cy > 0:
+                    x = self.tcursor.end.cy
+                    self.lines[x], self.lines[x - 1] = self.lines[x - 1], self.lines[x]
+                    self.tcursor.up(self.lines)
+                elif ch == ALT + Keys.DOWN and not self.tcursor.is_selection() and self.tcursor.end.cy < self.nlines - 1:
+                    x = self.tcursor.end.cy + 1
+                    self.lines[x], self.lines[x - 1] = self.lines[x - 1], self.lines[x]
+                    self.tcursor.down(self.lines)
 
-            elif ch.isprintable():
-                self.cursor_regulate()
-                if self.tcursor.is_selection():
-                    self.tcursor.empty_selection(self.lines)
-                self.lines[self.cy] = self.lines[self.cy][: self.cx] + ch.key + self.lines[self.cy][self.cx :]
-                self.tcursor.right(self.lines)
-            elif ch == Keys.BACKSPACE:
-                self.cursor_regulate()
-                if self.tcursor.is_selection():
-                    self.tcursor.empty_selection(self.lines)
-                elif self.cx > 0:
-                    self.lines[self.cy] = self.lines[self.cy][: self.cx - 1] + self.lines[self.cy][self.cx :]
-                    self.tcursor.left(self.lines)
-                elif self.cy > 0:
-                    self.tcursor.left(self.lines)
-                    self.lines[self.cy] += self.lines.pop(self.cy + 1)
-            elif ch == Keys.DEL:
-                self.cursor_regulate()
-                if self.tcursor.is_selection():
-                    self.tcursor.empty_selection(self.lines)
-                elif self.cx < len(self.cline):
-                    self.lines[self.cy] = self.lines[self.cy][: self.cx] + self.lines[self.cy][self.cx + 1 :]
-                elif self.cy < self.nlines - 1:
-                    self.lines[self.cy] += self.lines.pop(self.cy + 1)
-            elif ch == Keys.ENTER:
-                self.cursor_regulate()
-                if self.tcursor.is_selection():
-                    self.tcursor.empty_selection(self.lines)
-                pre = self.lines[self.cy][: self.cx]
-                post = self.lines[self.cy][self.cx :]
-                self.lines[self.cy] = pre
-                self.lines.insert(self.cy + 1, post)
-                self.tcursor.down(self.lines)
-            elif ch == CTRL + "c":
-                if self.tcursor.is_selection():
-                    text = self.tcursor.get_selection_text(self.lines)
-                else:
-                    text = self.lines[self.cy]
-                text = text.encode()
-                encoded = base64.b64encode(text).decode()
-                sys.stdout.write(f"\x1b]52;c;{encoded}\007")
-                sys.stdout.flush()
-            elif ch == CTRL + "x":
-                if self.tcursor.is_selection():
-                    text = self.tcursor.get_selection_text(self.lines)
-                    self.tcursor.empty_selection(self.lines)
-                else:
-                    text = self.lines[self.cy]
-                    if len(self.lines) > 0:
-                        self.lines.pop(self.cy)
+                elif ch.isprintable():
+                    self.cursor_regulate()
+                    if self.tcursor.is_selection():
+                        self.tcursor.empty_selection(self.lines)
+                    self.lines[self.cy] = self.lines[self.cy][: self.cx] + ch.key + self.lines[self.cy][self.cx :]
+                    self.tcursor.right(self.lines)
+                elif ch == Keys.BACKSPACE:
+                    self.cursor_regulate()
+                    if self.tcursor.is_selection():
+                        self.tcursor.empty_selection(self.lines)
+                    elif self.cx > 0:
+                        self.lines[self.cy] = self.lines[self.cy][: self.cx - 1] + self.lines[self.cy][self.cx :]
+                        self.tcursor.left(self.lines)
+                    elif self.cy > 0:
+                        self.tcursor.left(self.lines)
+                        self.lines[self.cy] += self.lines.pop(self.cy + 1)
+                elif ch == Keys.DEL:
+                    self.cursor_regulate()
+                    if self.tcursor.is_selection():
+                        self.tcursor.empty_selection(self.lines)
+                    elif self.cx < len(self.cline):
+                        self.lines[self.cy] = self.lines[self.cy][: self.cx] + self.lines[self.cy][self.cx + 1 :]
+                    elif self.cy < self.nlines - 1:
+                        self.lines[self.cy] += self.lines.pop(self.cy + 1)
+                elif ch == Keys.ENTER:
+                    self.cursor_regulate()
+                    if self.tcursor.is_selection():
+                        self.tcursor.empty_selection(self.lines)
+                    pre = self.lines[self.cy][: self.cx]
+                    post = self.lines[self.cy][self.cx :]
+                    self.lines[self.cy] = pre
+                    self.lines.insert(self.cy + 1, post)
+                    self.tcursor.down(self.lines)
+                elif ch == CTRL + "c":
+                    if self.tcursor.is_selection():
+                        text = self.tcursor.get_selection_text(self.lines)
                     else:
-                        self.lines = [""]
+                        text = self.lines[self.cy]
+                    text = text.encode()
+                    encoded = base64.b64encode(text).decode()
+                    sys.stdout.write(f"\x1b]52;c;{encoded}\007")
+                    sys.stdout.flush()
+                elif ch == CTRL + "x":
+                    if self.tcursor.is_selection():
+                        text = self.tcursor.get_selection_text(self.lines)
+                        self.tcursor.empty_selection(self.lines)
+                    else:
+                        text = self.lines[self.cy]
+                        if len(self.lines) > 0:
+                            self.lines.pop(self.cy)
+                        else:
+                            self.lines = [""]
 
-                    if self.cy >= self.nlines:
-                        self.tcursor.up(self.lines)
-                text = text.encode()
-                encoded = base64.b64encode(text).decode()
-                sys.stdout.write(f"\x1b]52;c;{encoded}\007")
-                sys.stdout.flush()
-            elif ch == ALT + SHIFT + Keys.UP and not self.tcursor.is_selection():
-                self.lines.insert(self.cy, self.lines[self.cy])
-            elif ch == ALT + SHIFT + Keys.DOWN and not self.tcursor.is_selection():
-                self.lines.insert(self.cy, self.lines[self.cy])
-                self.tcursor.down(self.lines)
-            elif ch == CTRL + "a":
-                self.tcursor.start.cy = 0
-                self.tcursor.start.cx = 0
-                self.tcursor.end.cy = len(self.lines) - 1
-                self.tcursor.end.cx = len(self.lines[-1])
+                        if self.cy >= self.nlines:
+                            self.tcursor.up(self.lines)
+                    text = text.encode()
+                    encoded = base64.b64encode(text).decode()
+                    sys.stdout.write(f"\x1b]52;c;{encoded}\007")
+                    sys.stdout.flush()
+                elif ch == ALT + SHIFT + Keys.UP and not self.tcursor.is_selection():
+                    self.lines.insert(self.cy, self.lines[self.cy])
+                elif ch == ALT + SHIFT + Keys.DOWN and not self.tcursor.is_selection():
+                    self.lines.insert(self.cy, self.lines[self.cy])
+                    self.tcursor.down(self.lines)
+                elif ch == CTRL + "a":
+                    self.tcursor.start.cy = 0
+                    self.tcursor.start.cx = 0
+                    self.tcursor.end.cy = len(self.lines) - 1
+                    self.tcursor.end.cx = len(self.lines[-1])
+
+        if ch == CTRL + "g":
+            self.undo()
+        if ch == CTRL + "y":
+            self.redo()
 
         # check if cursor is on screen
         if self.cy < self.scroll:
